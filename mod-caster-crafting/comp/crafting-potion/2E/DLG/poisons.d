92ca429237fe736a9885f93f14d9ad487c8f66cd
Erik Frantz <bardez@bardez.net>
2023-11-16 12:28:55 -0600

    Adding in optional poison state

---
+++ mod-caster-crafting/comp/crafting-potion/2E/DLG/poisons.d
@@ -0,0 +1,17 @@
+
+APPEND ~BZMSCAK~
+
+    IF ~~
+    THEN BEGIN bz_potion_brew_core_poison
+        SAY @30043  //~Which potion do you wish to brew?~
+            //Dynamic list of the potion registry for transitions
+
+            IF ~~
+            THEN REPLY @30025 // ~Pick another type of potion.~
+                GOTO bz_potion_brew_category_menu
+            IF ~~
+            THEN REPLY @30020 //Pack up your alchemy station
+                EXIT
+    END
+
+END
