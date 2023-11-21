9f2e99af723179ce9091b6315a6add10b5023b3f
Erik Frantz <bardez@bardez.net>
2023-11-21 00:30:31 -0600

    Updates and debugging

--- .research/GIMP batch/ie-inventory-shadow.py
+++ .research/GIMP batch/ie-inventory-shadow.py
@@ -1,25 +1,35 @@
+import os, glob, sys, time
 from gimpfu import *
 
 def ie_inventory_shadow(file, x_offset, y_offset, outputFolder):
     #Open the file; size to 64x64; duplicate layer; make black; offset 4, 5; set opacity to 50%; save to output
 
+    print "Opening '" + file + "' ..."
 
     # Indicates that the process has started.
-    gimp.progress_init("Opening '" + file + "'...")
+    gimp.progress_init("Opening '" + file + "' ...")
 
     try:
+        filename = os.path.basename(file)
+        # Use slicing to remove the extension
+        filename_without_ext = filename[:filename.rindex('.')]
+
+
         # Open file.
         fileImage = None
-        fileImage = pdb.gimp_file_load(infile, infile, run_mode=RUN_NONINTERACTIVE)
+        fileImage = pdb.gimp_file_load(file, file, run_mode=RUN_NONINTERACTIVE)
+        print "Opened '" + filename_without_ext + "' ..."
 
         #resize
-        resizedImage = pdb.gimp_image_scale(fileImage, 64, 64)
-        layer = resizedImage.layers[0]
+        pdb.gimp_context_set_interpolation(2) #INTERPOLATION-CUBIC (2)
+        pdb.gimp_image_scale(fileImage, 64, 64)
+        layer = fileImage.layers[0]
+        print "Resized '" + filename_without_ext + "' ..."
 
         # Create new layer.
-        newLayer = gimp.Layer(resizedImage, "Shadow", layer.width, layer.height, layer.type, layer.opacity, layer.mode)
+        newLayer = pdb.gimp_layer_new(fileImage, layer.width, layer.height, 1, "Shadow", layer.opacity, layer.mode) #RGBA-IMAGE (1)
         # the +1 adds it behind the top layer
-        resizedImage.add_layer(newLayer, +1)
+        fileImage.add_layer(newLayer, +1)
 
         # Put image into the new layer.
         pdb.gimp_edit_copy(layer)
@@ -31,20 +41,29 @@ def ie_inventory_shadow(file, x_offset, y_offset, outputFolder):
         newLayer.update(0, 0, newLayer.width, newLayer.height)
         # floating to layer
         pdb.gimp_floating_sel_to_layer(floating)
+        print "Duplicated '" + filename_without_ext + "' ..."
 
         #make layer black
         pdb.gimp_brightness_contrast(newLayer, -127, 127)
+        print "Shadowed '" + filename_without_ext + "' ..."
 
         #move the layer
         pdb.gimp_layer_set_offsets(newLayer, x_offset, y_offset)
+        print "Moved '" + filename_without_ext + "' ..."
 
         #set opacity
         pdb.gimp_layer_set_opacity(newLayer, 50)
-        flattened = pdb.gimp_image_flatten(resizedImage)
+        print "Set Opacity on '" + filename_without_ext + "' ..."
+
+        #flatten
+        flattened = pdb.gimp_image_flatten(fileImage)
+        print "Flattened '" + filename_without_ext + "' ..."
 
         # Export flattened image
-        target = outputFolder + "/" + filename + "s.png"
-        pdb.file_png_save_defaults(run_mode=RUN_NONINTERACTIVE, resizedImage, flattened, target, target)
+        target = outputFolder + "/" + filename_without_ext + "-s.png"
+        print "Saving '" + target + "' ..."
+        pdb.file_png_save_defaults(fileImage, flattened, target, target, run_mode=RUN_NONINTERACTIVE)
+        print "Saved '" + target + "'!"
 
 
     except Exception as err:
@@ -52,12 +71,21 @@ def ie_inventory_shadow(file, x_offset, y_offset, outputFolder):
 
 
 
-def run(directory):
-        start=time.time()
-        print "Running on directory \"%s\"" % directory
+def run(directory, x_offset, y_offset, outputFolder):
+        print "Run: directory : %s" % directory
+        print "Run: outputFolder : %s" % outputFolder
+        print "Run: x_offset : %s" % x_offset
+        print "Run: y_offset : %s" % y_offset
 
-        for infile in glob.glob(os.path.join(directory, '*.png')):
-                process(infile)
+        start=time.time()
+        print "Run: Running on directory \"%s\"" % directory
+        globPath = os.path.join(directory, '*.png')
+
+        print "looking at path: %s" % globPath
+
+        for infile in glob.glob(globPath):
+                print "Run: found file : %s" % infile
+                ie_inventory_shadow(infile, x_offset, y_offset, outputFolder)
         end=time.time()
         print "Finished, total processing time: %.2f seconds" % (end-start)
 
