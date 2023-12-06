bfb8eec6484ff497c9b7577ef8f8af05e9873700
Erik Frantz <bardez@bardez.net>
2023-12-06 09:52:11 -0600

    Add offense to the list

--- mod-caster-crafting/comp/crafting-potion/2E/DLG/core-potions.d
+++ mod-caster-crafting/comp/crafting-potion/2E/DLG/core-potions.d
@@ -1,6 +1,19 @@
 
 APPEND ~BZMSCAK~
     IF ~~
+    THEN BEGIN bz_potion_brew_core_cure
+        SAY @30043  //~Which potion do you wish to brew?~
+            //Dynamic list of the potion registry for transitions
+
+            IF ~~
+            THEN REPLY @30025 // ~Pick another type of potion.~
+                GOTO bz_potion_brew_category_menu
+            IF ~~
+            THEN REPLY @30020 //Pack up your alchemy station
+                EXIT
+    END
+
+    IF ~~
     THEN BEGIN bz_potion_brew_core_defense
         SAY @30043  //~Which potion do you wish to brew?~
             //Dynamic list of the potion registry for transitions
@@ -14,7 +27,7 @@ APPEND ~BZMSCAK~
     END
 
     IF ~~
-    THEN BEGIN bz_potion_brew_core_enhancement
+    THEN BEGIN bz_potion_brew_core_offense
         SAY @30043  //~Which potion do you wish to brew?~
             //Dynamic list of the potion registry for transitions
 
@@ -27,7 +40,7 @@ APPEND ~BZMSCAK~
     END
 
     IF ~~
-    THEN BEGIN bz_potion_brew_core_cure
+    THEN BEGIN bz_potion_brew_core_enhancement
         SAY @30043  //~Which potion do you wish to brew?~
             //Dynamic list of the potion registry for transitions
 
