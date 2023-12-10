aa4c22bf8ad3442a8feda767d8d6deceafe76b95
Erik Frantz <bardez@bardez.net>
2023-12-10 00:40:14 -0600

    Use hyphens in states

--- mod-caster-crafting/comp/crafting-potion/2E/DLG/poisons.d
+++ mod-caster-crafting/comp/crafting-potion/2E/DLG/poisons.d
@@ -2,13 +2,13 @@
 APPEND ~BZMSCAK~
 
     IF ~~
-    THEN BEGIN bz_potion_poison
+    THEN BEGIN ~bz-potion-poison~
         SAY @30043  //~Which potion do you wish to brew?~
             //Dynamic list of the potion registry for transitions
 
             IF ~~
             THEN REPLY @30025 // ~Pick another type of potion.~
-                GOTO bz_potion_main_menu
+                GOTO ~bz-potion-main_menu~
             IF ~~
             THEN REPLY @30020 //Pack up your alchemy station
                 EXIT
