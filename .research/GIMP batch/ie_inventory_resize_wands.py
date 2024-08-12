33d6319b58cba7bfb7004df46dd348dfa8ca2639
Erik Frantz <bardez@bardez.net>
2024-08-12 11:54:03 -0500

    Resize to the target resolution, THEN crop it to prevent any scaling issues

--- .research/GIMP batch/ie_inventory_resize_wands.py
+++ .research/GIMP batch/ie_inventory_resize_wands.py
@@ -3,7 +3,7 @@ from gimpfu import *
 
 #this script will crop the image to the top-left 75% of the image (so focusing on the head of the wands) and then resize
 
-def ie_inventory_resize(file, outputFolder, cropScale):
+def ie_inventory_resize(file, outputFolder):
     #Open the file; size to 64x64; duplicate layer; make black; offset 4, 5; set opacity to 50%; save to output
 
     print "Opening '" + file + "' ..."
@@ -21,20 +21,14 @@ def ie_inventory_resize(file, outputFolder, cropScale):
         fileImage = pdb.gimp_file_load(file, file, run_mode=RUN_NONINTERACTIVE)
         print "Opened '" + filename_without_ext + "' ..."
 
-        #crop to 75%
-        width = pdb.gimp_image_width(fileImage)
-        height = pdb.gimp_image_height(fileImage)
-        newWidth = width * cropScale
-        newHeight = height * cropScale
-        cropWidth = int(newWidth)
-        cropHeight = int(newHeight)
-        pdb.gimp_image_crop(fileImage, cropWidth, cropHeight, 0, 0)
-
         #resize
         pdb.gimp_context_set_interpolation(2) #INTERPOLATION-CUBIC (2)
-        pdb.gimp_image_scale(fileImage, 64, 64)
-        layer = fileImage.layers[0]
+        pdb.gimp_image_scale(fileImage, 96, 96) #150% of inventory icon
         print "Resized '" + filename_without_ext + "' ..."
+
+        #crop to inventory size
+        pdb.gimp_image_crop(fileImage, 64, 64, 0, 0)
+        layer = fileImage.layers[0]
 
         # Export flattened image
         target = outputFolder + "/" + filename_without_ext + "-i.png"
@@ -48,10 +42,9 @@ def ie_inventory_resize(file, outputFolder, cropScale):
 
 
 
-def run(directoryIn, directoryOut, cropScale):
+def run(directoryIn, directoryOut):
         print "Run: directoryIn : %s" % directoryIn
         print "Run: directoryOut : %s" % directoryOut
-        print "Run: cropScale : %s" % cropScale
 
         start=time.time()
         print "Run: Running on directory \"%s\"" % directoryIn
@@ -61,7 +54,7 @@ def run(directoryIn, directoryOut, cropScale):
 
         for infile in glob.glob(globPath):
                 print "Run: found file : %s" % infile
-                ie_inventory_resize(infile, directoryOut, cropScale)
+                ie_inventory_resize(infile, directoryOut)
         end=time.time()
         print "Finished, total processing time: %.2f seconds" % (end-start)
 
