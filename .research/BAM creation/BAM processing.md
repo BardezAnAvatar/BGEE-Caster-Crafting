e0d6cdec73fcf1238342e86115b141728a5db121
Erik Frantz <39980629+BardezAnAvatar@users.noreply.github.com>
2025-12-21 13:30:51 -0600

    BAM generation scripts for wands (#99)
    * Stab at a BAM generation script for wands
    
    * disabling bam generation. Debugging.
    
    * Fixed it generating BAMD files properly
    
    * Add in instructions for running the wands generation
    
    * Renaming method
    
    * Get the command line params working
    
    * Disable debugging arguments statement and instead invoking the console app
    
    * Rename the generation file
    
    * Stab at script for generating description BAM files
    
    * Fixing the script
    
    * Adding a description of how to run the description script
    
    * Looks like my sequence was off for the frame cycles. Oops.
    
    * Set the BAM Profile, per @Sam (owner of PS Bam)
    
    * try to disable trimming frame data:
    
    > Trimmed Frames in 0.005272 sec.
    >  I: Trimmed 1871 Pixels from FrameData.
    
    * flip the sequence/cycle
    
    * Add the BAMProfile in
    
    * Disable the BAMProfile ItemIcon, though this seems pointless, now
    
    * Restore the ItemIcon profile because it was not the cause of my woes

--- .research/BAM creation/BAM processing.md
+++ .research/BAM creation/BAM processing.md
@@ -15,3 +15,20 @@ To execute my script locally:
 ```
  .\scroll_bam_generation.ps1 -Bammer "C:\Projects\Game Mods\Baldur's Gate\Tools\bammer\bammer.exe" -PsBam "C:\Projects\Game Mods\Baldur's Gate\Tools\PS-BAM\PS BAM_x64.exe" -RolledFrame "C:\temp\BAM\out\bammer\bammer_000.png" -DirInputPng "C:\temp\BAM\in" -DirOutputTemp "C:\temp\BAM\intermediate" -DirOutputBam "C:\temp\BAM\out"
 ```
+
+### Wands
+```
+cd "C:\Projects\Game Mods\Baldur's Gate\Caster Crafting\.research\BAM creation"
+```
+
+```
+.\wand_inventory_bam_generation.ps1 -PsBam "C:\Projects\Game Mods\Baldur's Gate\Tools\PS-BAM\PS BAM_x64.exe" -PostfixInventory "-i.png" -PostfixFloating "-s.png" -DirInputInventory "C:\temp\BAM\render\inventory" -DirInputShadow "C:\temp\BAM\render\float" -DirOutputTemp "C:\temp\BAM\intermediate" -DirOutputBam "C:\temp\BAM\out"
+```
+
+```
+cd "C:\Projects\Game Mods\Baldur's Gate\Caster Crafting\.research\BAM creation"
+```
+
+```
+.\wand_description_bam_generation.ps1 -PsBam "C:\Projects\Game Mods\Baldur's Gate\Tools\PS-BAM\PS BAM_x64.exe" -DirInputPng "C:\Projects\Game Mods\Baldur's Gate\Caster Crafting\.assets\wands\output\render\description" -DirOutputTemp "C:\temp\BAM\intermediate" -DirOutputBam "C:\temp\BAM\out"
+```
---
+++ .research/BAM creation/wand_inventory_bam_generation.ps1
@@ -0,0 +1,189 @@
+<#
+.SYNOPSIS
+    This script will mix Infinity Engine wand PNG files into best-compression BAM files
+.DESCRIPTION
+    This script will run through an input directory, joining individual *.png files in `DirInputPng` into BAM-D pseudo-script descriptive files which will be output to `DirOutputTemp`; these BAM-D files will be merged into BAM files via `psbam`; all such *.BAM files will be compressed via `PsBam` executable and emitted into the `DirOutputBam` directory
+.PARAMETER PsBam
+    The path to the `PS BAM` executable on disk.
+    Acquire from: https://github.com/Sampsca/PS-BAM
+.PARAMETER PostfixInventory
+    The string post-fix of a filename indicating an inventory *.png file
+.PARAMETER PostfixFloating
+    The string post-fix of a filename indicating an carried/picked up/floating *.png file
+.PARAMETER DirInputInventory
+    The path to the directory containing all of the PNG files to display in inventory slots intended to convert into BAMs
+.PARAMETER DirInputShadow
+    The path to the directory containing all of the PNG files to display when dragged intended to convert into BAMs
+.PARAMETER DirOutputTemp
+    The path to the directory where intermediate uncompressed BAMs can be stored
+.PARAMETER DirOutputBam
+    The path to the directory where final output should reside
+.EXAMPLE
+    C:\Work\BAM>'.\wand_bam_generation.ps1'
+    >>> -PsBam 'C:\bin\psbam\psbam.exe'
+    >>> -PostfixInventory '-i.png'
+    >>> -PostfixFloating '-f.png'
+    >>> -DirInputInventory 'C:\Work\Bam\Input\Todo1\'
+    >>> -DirInputShadow 'C:\Work\Bam\Input\Todo2\'
+    >>> -DirOutputTemp 'C:\Work\Bam\Temp\'
+    >>> -DirOutputBam 'C:\Work\Bam\Output\'
+    <This will open up all the *.png files in -DirInputPng, create a BAM-D for them then convert/compress that BAM
+    using PS BAM>
+.NOTES
+    Author: Bardez
+    Date:   August 12, 2024
+#>
+param(
+    [Parameter(Mandatory)]
+    [string]$PsBam,
+
+    [Parameter(Mandatory)]
+    [string]$PostfixInventory,
+
+    [Parameter(Mandatory)]
+    [string]$PostfixFloating,
+
+    [Parameter(Mandatory)]
+    [string]$DirInputInventory,
+
+    [Parameter(Mandatory)]
+    [string]$DirInputShadow,
+
+    [Parameter(Mandatory)]
+    [string]$DirOutputTemp,
+
+    [Parameter(Mandatory)]
+    [string]$DirOutputBam
+)
+
+
+# this function will generate BAM-D input/output
+Function GenerateBamD
+{
+    param(
+        [Parameter(Mandatory)]
+        [string]$InventoryFrame,
+
+        [Parameter(Mandatory)]
+        [string]$FloatingFrame
+    )
+
+    $bamd =@"
+frame f00000 `"$FloatingFrame`" 32 32
+frame f00001 `"$InventoryFrame`" 0 0
+
+sequence f00000 f00001
+"@
+
+    return $bamd
+}
+
+function Invoke-Command()
+{
+    param
+    (
+        [string]$program = $(throw "Please specify a program" ),
+        [string]$argumentString = "",
+        [switch]$waitForExit
+    )
+
+    $psi = new-object "Diagnostics.ProcessStartInfo"
+    $psi.FileName = $program
+    $psi.Arguments = $argumentString
+    $proc = [Diagnostics.Process]::Start($psi)
+    if ( $waitForExit )
+    {
+        $proc.WaitForExit();
+    }
+}
+
+#this function will take a file in the temp dir and compress it using PS-BAM
+Function GenerateBam
+{
+    param(
+        [Parameter(Mandatory)]
+        [string]$PathPsBam,
+
+        [Parameter(Mandatory)]
+        [string]$DirInput,
+
+        [Parameter(Mandatory)]
+        [string]$DirOutput,
+
+        [Parameter(Mandatory)]
+        [string]$BamdName
+    )
+
+    #invoke PS-BAM
+    $params = @(
+        "--CompressionProfile", "`"Recommended`"",
+        "--DebugLevelL", "1",
+        "--DebugLevelP", "2",
+        "--DebugLevelS", "1",
+        "--LogFile", "`"$DirOutput\$BamdName.log`"",
+        "--OutPath", "`"$DirOutput`"",
+        "--Save", "`"BAM`"",
+        "--BAMProfile", "`"ItemIcon`"",    #this seems to mess up the rendering in-game, now
+        "--TrimFrameData", "0"
+        "`"$DirOutputTemp\$BamdName`""
+        )
+
+    $arguments = $params -join " "
+
+    #debug
+    #Write-Host $arguments
+
+    Invoke-Command -program $PathPsBam -argumentString $arguments -waitForExit true
+}
+
+Function Main
+{
+    $start = Get-Date
+
+    Write-Host "Generating BAM-Ds ..." -ForegroundColor red
+
+    #iterate through all of the input DIR PNGs
+    $filter = '*' + $PostfixInventory
+    $pngFiles = Get-ChildItem $DirInputInventory -Filter $filter
+    foreach ($file in $pngFiles)
+    {
+        $baseName = ($file.Name).Replace($PostfixInventory, '')
+        $floatingFile = ($file.FullName).Replace($PostfixInventory, $PostfixFloating).Replace($DirInputInventory, $DirInputShadow)
+
+        $bamdName = "$($baseName).BAMD"
+
+        Write-Host "Generating $bamdName from $file ..." -ForegroundColor blue
+
+        #generate BAMD
+        $bamdContent = GenerateBamD $file.FullName $floatingFile
+        $bamdContent = $bamdContent.Replace("\", "/") #apparently Bammer needs this syntax?
+
+        #Write the file
+        Set-Content -Path "$($DirOutputTemp)\$($bamdName)" -Value $bamdContent
+    }
+
+    Write-Host "Optimizing BAMs ..." -ForegroundColor red
+
+    #iterate through all temp dir BAMDs
+    $bamdFiles = Get-ChildItem $DirOutputTemp | Where-Object { $_.Name -like "*.BAMD" }
+    foreach($file in $bamdFiles)
+    {
+        $bamName = ($file.Name).Replace('.BAMD', '.BAM')
+
+        Write-Host "Generating $bamName ..." -ForegroundColor Magenta
+
+        #Generate BAM
+        GenerateBam -PathPsBam $PsBam -DirInput $DirOutputTemp -DirOutput $DirOutputBam -BamdName $file
+
+        #this locked up my processor and OS, so... WAIT
+        Start-Sleep -Milliseconds 20
+    }
+
+    $end = Get-Date
+
+    $duration = $end - $start
+
+    Write-Host "Script duration: $duration ..." -ForegroundColor Yellow
+}
+
+Main
